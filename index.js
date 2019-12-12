068b763919b16ff39737732d3e447d80480b1c62
Goricanec David <David.Goricanec@andritz.com>
2019-12-12 17:17:31 +0100

    first post worked YEAH!

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 //var Student = require('./student');
 var mongoose = require('mongoose');
 var cors = require('cors');
+var qs = require('querystring');
 
 const connectToDB = () => {
   mongoose
@@ -47,14 +48,13 @@ app.get('/', function (req, res) {
 
 app.get('/getStudent', function (req, res) {
 
-	console.log(req.query);
+  //console.log(req.query);
   var query = { "_id" :  req.query.id };
-  console.log("Searching for student with id: " + req.query.id);
+  //console.log("Searching for student with id: " + req.query.id);
   Student.find(query)
     .then((students) => {
-      console.log(students);
+      //console.log(students);
 	  res.json(students);
-	  
     })
 
 
@@ -62,24 +62,33 @@ app.get('/getStudent', function (req, res) {
 
 
 app.post('/updateStudent', function (req, res) {
-  /*console.log(req.param);
-  console.log(req.params("id"));
-  console.log(req.body("id"));
-  console.log(req.body.id)
-  console.log(req.query);*/
-  //console.log(req);
-  var myquery = { "_id": req.params.id };
-  var newvalues = { $set: {"id"          : req.params.Student_id
-                         , "vorname"     : req.params.vorname
-						// , "nachname"    : req.params.nachname
-						// , "studiengang" : req.params.studiengang
-						// , "wohnort"     : req.params.wohnort
-						// , "semester"    : req.params.semester
-						} };
-  Student.updateOne(myquery, newvalues, function(err, res) {
-    if (err) throw err;
-    console.log("1 Student updated");
-  });
+    var body = '';
+	req.on('data', function (data) {
+		body += data;
+
+		// Too much POST data, kill the connection!
+		// 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+		if (body.length > 1e6)
+			req.connection.destroy();
+	});
+
+	req.on('end', function () {
+		  var post = qs.parse(body);
+		  var myquery = { "_id": post['id'] };
+		  var newvalues = { $set: {"id"          : post['Student_id']
+								 , "vorname"     : post['vorname']
+								// , "nachname"    : req.params.nachname
+								// , "studiengang" : req.params.studiengang
+								// , "wohnort"     : req.params.wohnort
+								// , "semester"    : req.params.semester
+								} };
+		  Student.updateOne(myquery, newvalues, function(err, res) {
+			if (err) throw err;
+			console.log("1 Student updated");
+		  });
+	});
+  
+
 
 
 });
